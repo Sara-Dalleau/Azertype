1a3b6a4927ec70d564166fd6a8355b00a649bc93
Dalleau Sara <sara.dalleau@gmail.com>
2025-11-27 20:49:54 +0100

    recupérer élément dom

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
 // lancerJeu();
+
+// Récupération des éléments du DOM grâce a leur ID
+let BaliseinputEcriture = document.getElementById("inputEcriture");
+console.log(inputEcriture);
+
+let BalisebuttonEcriture = document.getElementById("btnValiderMot");
+console.log(btnValiderMot);
+
+// Récupération des éléments du DOM grâce a querySelector
+
+let BaliseZoneProposition = document.querySelector(".zoneProposition");
+console.log(BaliseZoneProposition);
+
+let BaliseZoneScore = document.querySelector(".zoneScore span");
+console.log(BaliseZoneScore);
+
+// Récupération des éléments du DOM grâce a querySelectorAll
+let BaliseOptionSource = document.querySelectorAll(".optionSource input");
+console.log(BaliseOptionSource);
